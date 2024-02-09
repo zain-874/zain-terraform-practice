adecf50af80c924df2cb0b934260ca12ce90b0a8
Zain Ul Abedin <zabedin@coresense.com>
2024-02-09 17:23:05 +0500

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web-server" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform testing..."
+  }
+}
+
