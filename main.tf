b65829a39270703012c5994bbf1f29c887b43688
Zain Ul Abedin <zabedin@coresense.com>
2024-02-11 09:19:49 +0500

    simplest webserver with a security group

--- main.tf
+++ main.tf
@@ -7,7 +7,28 @@ resource "aws_instance" "web-server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraform testing..."
+    Name = "web-server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+  name = "web-server-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
