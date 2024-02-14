bc11368085b34182a117a5f0a5ce52b980999bd4
Zain Ul Abedin <zabedin@coresense.com>
2024-02-14 23:27:24 +0500

    add input/output vars

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "web-server" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -25,10 +25,20 @@ resource "aws_security_group" "instance" {
   name = "web-server-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.web-server.public_ip
+  description = "The public IP address of the web server"
+}
